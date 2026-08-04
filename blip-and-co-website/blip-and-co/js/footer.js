e19cd333e3163a490c3080654960385b42ab681d
ulimorasanchez <ulimorasanchez@gmail.com>
2026-08-04 12:21:50 -0400

    Update footer.js

--- blip-and-co-website/blip-and-co/js/footer.js
+++ blip-and-co-website/blip-and-co/js/footer.js
@@ -1,34 +1,33 @@
 document.getElementById("footer").innerHTML = `
   <div class="container footer-grid">
 
-    <div>
-      <div class="footer-brand">
-        <img src="img/daxterlogo.jpg" class="brand-icon"> Daxter Design
-      </div>
-      <p>Art, animation, stickers, and creative chaos.</p>
+    <div class="footer-brand">
+      <img src="img/daxterlogo.jpg" class="brand-icon">
+      <span>Daxter Design</span>
     </div>
 
-    <div>
-      <h4>Links</h4>
-      <a href="index.html">Home</a>
-      <a href="videos.html">Videos</a>
-      <a href="shop.html">Sticker Shop</a>
-      <a href="about.html">About</a>
-      <a href="updates.html">Updates</a>
-      <a href="support.html">Support</a>
-    </div>
+    <div class="footer-social">
 
-    <div>
-      <h4>Social</h4>
-      <a href="https://www.youtube.com/@DaxterAStudio" target="_blank">YouTube</a>
-      <a href="https://www.instagram.com/daxterstudio/" target="_blank">Instagram</a>
-      <a href="https://www.tiktok.com/@daxterstudio" target="_blank">TikTok</a>
-    </div>
+      <a href="https://www.tiktok.com/@daxterstudio" target="_blank" class="social-link">
+        <img src="assets/icons/tiktok.svg" alt="TikTok">
+        <span>@daxterstudio</span>
+      </a>
 
-    <div>
-      <h4>Shop & Support</h4>
-      <a href="https://www.etsy.com/shop/DaxterDesigns" target="_blank">Etsy Shop</a>
-      <a href="https://ko-fi.com/daxterstudio" target="_blank">Ko‑fi Donations</a>
+      <a href="https://www.youtube.com/@DaxterAStudio" target="_blank" class="social-link">
+        <img src="assets/icons/youtube.svg" alt="YouTube">
+        <span>@DaxterAStudio</span>
+      </a>
+
+      <a href="https://www.instagram.com/daxterstudio/" target="_blank" class="social-link">
+        <img src="assets/icons/instagram.svg" alt="Instagram">
+        <span>@daxterstudio</span>
+      </a>
+
+      <a href="https://www.etsy.com/shop/DaxterDesigns" target="_blank" class="social-link">
+        <img src="assets/icons/etsy.svg" alt="Etsy">
+        <span>DaxterDesigns</span>
+      </a>
+
     </div>
 
   </div>
